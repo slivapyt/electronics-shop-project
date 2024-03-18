e81d1b92dbcafc1eec9874368ad35aebfc4fc723
slivapy <faer2325@gamil.com>
2024-03-18 18:32:43 +0700

    ss

--- src/item.py
+++ src/item.py
@@ -3,28 +3,27 @@ class Item:
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
+
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
+        name: Название товара.
+        price: Цена за единицу товара.
+        quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate

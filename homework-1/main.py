664eabb04e3c3ccf15c90b724616ad42a10f5a1c
slivapy <faer2325@gamil.com>
2024-03-18 18:09:18 +0700

    branch

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,8 +1,8 @@
 from src.item import Item
-
 if __name__ == '__main__':
     item1 = Item("Смартфон", 10000, 20)
     item2 = Item("Ноутбук", 20000, 5)
+    item3 = Item("жопич", 50000, 2)
 
     print(item1.calculate_total_price())  # 200000
     print(item2.calculate_total_price())  # 100000
@@ -11,8 +11,11 @@ if __name__ == '__main__':
     Item.pay_rate = 0.8
     # применяем скидку
     item1.apply_discount()
+    item3.apply_discount()
 
     print(item1.price)  # 8000.0
     print(item2.price)  # 20000
+    # print(item3.price)  # 40000.0
 
-    print(Item.all)  # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    # [<__main__.Item object at 0x000001EC6250C690>, <__main__.Item object at 0x000001EC6250C6D0>]
+    print(Item.all)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def number5():
+    return Item("жопич", 50000, 2)
+
+
+def test_calculate_total_price(number5):
+    """На расчет суммы конкретного товара в магазине"""
+    assert number5.calculate_total_price() == 100000
+
+
+def test_apply_discount(number5):
+    number5.pay_rate = 0.8
+    number5.apply_discount()
+    assert number5.price == 40000.0
